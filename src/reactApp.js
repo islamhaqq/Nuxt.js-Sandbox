d82f03388bf8df747da21320732be6cf72e58c6c
Syed Aman <sna274@nyu.edu>
2016-10-26 14:04:52 -0400

    render HTML in a React component

--- src/reactApp.js
+++ src/reactApp.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+class App extends React.Component{
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
 render()
