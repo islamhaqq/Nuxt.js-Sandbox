3d3d035ed388d9861d87e5ca8428dde175a76506
Syed Aman <sna274@nyu.edu>
2016-10-28 16:53:36 -0400

    create file to render and store list of contacts

---
+++ src/contactsList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ContactList extends React.Component {
+    render() {
+        return (
+            <ul>
+                <li>Scot 555 555 5555</li>
+            </ul>
+        )
+    }
+}
